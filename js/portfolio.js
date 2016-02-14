716b81e34b68dc8bdd15cf753d00a2e7f77090a4
timothy <tim.longoria@gmail.com>
2016-02-14 11:44:53 -0600

    Added portfolio footer

---
+++ js/portfolio.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  // mini portfolio navigation
+  $('#ab').click(function(){
+    $('#about').toggle(600);
+  });
+ 
+  $('#pr').click(function(){
+    $('#projects').toggle(600);
+  });
+
+  $('#ti').click(function(){
+    $('#timpic').toggle(600);
+  });
+
+});
